8cf7874d07aaa777ff1947144bb75f90f39e16f1
Elias Nijs <elias.nijs@gmail.com>
2023-06-08 23:24:33 +0200

    :memo: report

--- src/flowsomtool.py
+++ src/flowsomtool.py
@@ -18,6 +18,7 @@ from fst_windows import (
 
 
 def main():
+    # Setup Gui context
     sdl2.SDL_Init(sdl2.SDL_INIT_EVERYTHING)
     window = sdl2.SDL_CreateWindow(
         b"FlowSOMTool",
@@ -32,12 +33,15 @@ def main():
     imgui.core.style_colors_light()
     renderer = imgui.integrations.sdl2.SDL2Renderer(window)
 
+    # Setup flowsom context
     file_paths = []
     flowsom = FlowSOM()
     image = Fst_ImageControls()
 
+    # Start program loop
     running = True
     while running:
+        # Handle input
         event = sdl2.SDL_Event()
         while sdl2.SDL_PollEvent(ctypes.byref(event)) != 0:
             if event.type == sdl2.SDL_QUIT:
@@ -45,15 +49,17 @@ def main():
             renderer.process_event(event)
         renderer.process_inputs()
 
+        # Get window context
         width = ctypes.c_int(0)
         height = ctypes.c_int(0)
         window_dim = sdl2.SDL_GetWindowSize(window, width, height)
         window_dim = (width.value, height.value)
 
+        # Setup plot
         texture_id = gl.glGenTextures(1)
         prev_plot = image.i_plot
 
-        # Imgui
+        # Configure imgui windows
         imgui.new_frame()
         win_actions(window_dim, flowsom, file_paths, image)
         win_parameters(
@@ -99,9 +105,10 @@ def main():
         renderer.render(imgui.get_draw_data())
         sdl2.SDL_GL_SwapWindow(window)
 
-        # Destroy resources
+        # Cleanup resources
         gl.glDeleteTextures(texture_id)
 
+    # Cleanup resources
     sdl2.SDL_GL_DeleteContext(gl_context)
     sdl2.SDL_DestroyWindow(window)
     sdl2.SDL_Quit()
